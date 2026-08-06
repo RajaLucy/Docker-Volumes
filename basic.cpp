d018e3de5692ae9b7eed6a63a919dfcfe48e9ecc
RajaLucy <133349260+RajaLucy@users.noreply.github.com>
2026-08-06 09:10:59 +0530

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+using namespace std;
+
+void printnumbers(int n, int arr[n]){
+
+for (int i=0;i<=n;i++){
+cout << arr[i] << endl;
+}
+
+cout << "This is void Function for Printing numbers" << endl;
+}
